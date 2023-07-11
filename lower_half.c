1523931c4546b7292f27e24bb4931260acd4a565
Rajat Bisht <bisht.r@northeastern.edu>
2023-07-11 13:57:51 -0400

    Iterate over the array of stored function addresses using pointer arithmetics

--- lower_half.c
+++ lower_half.c
@@ -18,18 +18,22 @@ void bar(int j, int k)
 void (*fn_Arr[2])() __attribute__((section(".custom_section"))) = {&foo, &bar};
 int main()
 {
-	void (**fn_ptr)(int);	//declaring a pointer for function
+	void (**fn_ptr)();	//declaring a pointer for function
 	
-	fn_ptr = (void (**)(int)) 0x5000000;		//storing addr of foo in fn_ptr
-//	fn_Arr[0] = &foo;
-//	fn_Arr[1] = &bar;		
-	printf("Hello, this is main calling function foo by address...\n");
-//	fn_ptr(42);
-	(*fn_Arr[0])(23);
-	(*fn_Arr[1])(2, 3);
+	fn_ptr = (void (**)()) 0x5000000;		//storing addr of foo in fn_ptr
+	printf("\nHello, this is main calling function foo by address...\n");
+
+//	(*fn_Arr[0])(23);
+//	(*fn_Arr[1])(2, 3);
 	
 	printf("----------------------------------------------------------\n");
-	(*fn_ptr)(42);
+	(*fn_ptr)(23);
+	printf ("\n");
+
+        printf("Hello, this is main calling function foo by address...\n");
+        printf("----------------------------------------------------------\n");	
+	(*(fn_ptr + 1))(2, 3);
+	printf("\n");
 
 	return 0;
 
