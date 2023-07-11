9fc3a9df5dd09f3188a59967f7f9dabe4acafa93
Rajat Bisht <bisht.r@northeastern.edu>
2023-07-11 13:43:14 -0400

    Acessing the functions stored in array using the WELL-KNOWN address

--- lower_half.c
+++ lower_half.c
@@ -18,15 +18,18 @@ void bar(int j, int k)
 void (*fn_Arr[2])() __attribute__((section(".custom_section"))) = {&foo, &bar};
 int main()
 {
-	void (*fn_ptr)();	//declaring a pointer for function
+	void (**fn_ptr)(int);	//declaring a pointer for function
 	
-	fn_ptr = &foo;		//storing addr of foo in fn_ptr
+	fn_ptr = (void (**)(int)) 0x5000000;		//storing addr of foo in fn_ptr
 //	fn_Arr[0] = &foo;
 //	fn_Arr[1] = &bar;		
 	printf("Hello, this is main calling function foo by address...\n");
-//	(*fn_ptr)(23);
+//	fn_ptr(42);
 	(*fn_Arr[0])(23);
 	(*fn_Arr[1])(2, 3);
+	
+	printf("----------------------------------------------------------\n");
+	(*fn_ptr)(42);
 
 	return 0;
 
